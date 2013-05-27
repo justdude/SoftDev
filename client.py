5f7b618ad01d78346ee6839cd56e515337c576f5
ivan <dude.just.dude@gmail.com>
2013-05-27 22:17:39 +0400

    hello world
    тест гита

--- client.py
+++ client.py
@@ -3,3 +3,10 @@ Created on 25 05 2013
 
 @author: roma
 '''
+import socket
+
+
+Socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+Socket.bind("127.0.0.1",81)
+Socket.bind()
+print "Hello, World!"
