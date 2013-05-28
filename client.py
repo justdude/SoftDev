91b4a9c9d68d72c987e3f8dfb752255b1a56df63
ivan <dude.just.dude@gmail.com>
2013-05-28 23:35:47 +0400

    Сервер клиент

--- client.py
+++ client.py
@@ -7,7 +7,13 @@ import socket
 
 
 Socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-Socket.bind("127.0.0.1",81)
-Socket.bind()
-print "Hello, World!"
-print "Second hello world"
+Socket.connect(("127.0.0.1",81))
+
+while True:
+    str=raw_input("Enter string,please")
+    Socket.send(str)
+    data=Socket.recv(1024)
+    print 'Received: '+data
+
+Socket.close()
+
--- server.py
+++ server.py
@@ -1,5 +1,15 @@
-'''
-Created on 25 05 2013
+import socket
 
-@author: roma
-'''
+
+Socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+Socket.bind(("127.0.0.1",81))
+Socket.listen(100)
+conn, adr = Socket.accept()
+print "Connected", adr
+
+while True:
+    data=conn.recv(1024) 
+    if not data:
+        break
+    conn.send(data.upper())
+conn.close()
